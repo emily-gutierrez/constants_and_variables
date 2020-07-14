22452642ee1f932a9e0d4c61db74ad6e678ccae3
emily-gutierrez <67969028+emily-gutierrez@users.noreply.github.com>
2020-07-13 22:15:26 -0500

    Add files via upload

---
+++ Variables.playground/Pages/Exercise-Shopping List.xcplaygroundpage/Contents.swift
@@ -0,0 +1,25 @@
+/*:
+ ## Exercise: Making a Shopping List
+
+ The constants below represent some of the things you might want to add to a shopping list:
+*/
+let eggs = "Eggs"
+let milk = "Milk"
+let cheese = "Cheese"
+let bread = "Bread"
+let rice = "Rice"
+let newLine = "\n"
+//: - callout(Exercise): Create a string variable with an initial value of `""`. Add each constant item above to the list, one at a time. Add a `newLine` in between each item. Remember you can join two strings using the `+` operator.
+var string = ""
+var Shopping += list
+var Shopping += newLine
+var Shopping += "eggs"
+var Shopping += newLine
+var Shopping += "milk"
+var Shopping += newLine
+var Shopping += "cheese"
+var Shopping += newLine
+var Shopping += "bread"
+var Shopping += newLine
+var Shopping += "rice"
+//: [Previous](@previous)  |  page 12 of 13  |  [Next: Exercise: 501](@next)
---
+++ Variables.playground/Pages/You Make the Choice.xcplaygroundpage/Contents.swift
@@ -0,0 +1,6 @@
+
+let name = "John"
+/*:
+Always remember that Xcode is a tool that is trying to give you a helpful suggestion, but it’s just a suggestion. Don’t accept a Fix-it without taking a moment to understand the change it is making.
+ */
+//: [Previous](@previous)  |  page 8 of 13  |  [Next: Safer Code in a Varying World](@next)
